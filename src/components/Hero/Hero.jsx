b0a75a1a1d1cf03b972c46d04d554d4701054dde
unknown <yeasin.mohammod018@gmail.com>
2024-10-18 11:34:53 +0600

    content change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
         <div className="">
             <div className="flex items-center flex-wrap px-2 md:px-0">
                 <div className="relative lg:w-6/12 lg:py-24 xl:py-32">
-                    <h1 className="text-2xl text-gray-300 md:text-3xl lg:w-10/12">Your favorite BayLeaf Restaurant, is now in NORWOOD</h1>
+                    <h1 className="text-2xl text-gray-300 md:text-3xl lg:w-10/12">Your favorite FoodLeaf Restaurant, is now in NewYork</h1>
                     <div className="w-full mt-12">
                         <p className="text-white font-bold lg:w-10/12 md:text-5xl text-xl">We do Corporate Events, Birthday Functions, Year Ending Functions & Outdoor Catering.</p>
                         <button className="btn btn-ghost btn-lg md:mt-16 mt-6 bg-red-500 text-white px-12 text-xl hover: border hover:border-red-500 hover:bg-transparent hover:text-red-500">Get Started</button>
